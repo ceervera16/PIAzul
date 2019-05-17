c98905b990976be1c49604b45401fd7600b2f725
ALUMNADO\rumalo01 <rubenmartinezlozano@gmail.com>
2019-05-17 20:02:53 +0200

    Avance en clase administrador

--- CheapMarket/CheapMarket/Administrador.cs
+++ CheapMarket/CheapMarket/Administrador.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace CheapMarket
 {
@@ -32,19 +33,81 @@ namespace CheapMarket
         }
 
         //Métodos
-        public void AgregarProducto()
-        {
 
+        /// <summary>
+        /// Añade un producto a la base de datos
+        /// </summary>
+        /// <param name="conexion">Conexión con la base de datos</param>
+        /// <param name="prod">Producto a insertar</param>
+        /// <returns></returns>
+        public int AgregarProducto(MySqlConnection conexion, Productos prod)
+        {
+            string consulta;
+            if (prod.Info=="")
+            {
+                consulta = string.Format("INSERT INTO productos(Codigo,Nombre,Precio,Descripcion,Categoria" +
+                "VALUES ('{0}','{1}','{2}','{3}','{4}','{5}'", prod.Codigo, prod.Nombre, prod.Precio, prod.Descripcion, prod.Categoria);
+            }
+            else
+            {
+                consulta = string.Format("INSERT INTO productos(Codigo,Nombre,Precio,Descripcion,Categoria,InformacionNutritiva" +
+                "VALUES ('{0}','{1}','{2}','{3}','{4}','{5}'", prod.Codigo, prod.Nombre, prod.Precio, prod.Descripcion, prod.Categoria, prod.Info);
+            }
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            return comando.ExecuteNonQuery();
         }
 
-        public void BorrarProducto()
+        /// <summary>
+        /// Borra un producto de la base de datos
+        /// </summary>
+        /// <param name="conexion">Conexión con la base de datos</param>
+        /// <param name="codigo">Código del producto a borrar</param>
+        /// <returns>Número de registros afectados</returns>
+        public int BorrarProducto(MySqlConnection conexion, string codigo)
         {
-
+            string consulta;
+            consulta = string.Format("DELETE FROM productos WHERE codigo = '{0}'",codigo);
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            return comando.ExecuteNonQuery();
         }
 
-        public void EditarProducto()
+        /// <summary>
+        /// Edita la información de un producto de la base de datos
+        /// </summary>
+        /// <param name="conexion">Conexión con la base de datos</param>
+        /// <param name="codigo">Código del producto</param>
+        /// <param name="nombre">Nuevo nombre del producto</param>
+        /// <param name="precio">Nuevo precio del producto</param>
+        /// <param name="descripcion">Nueva descripcion del producto</param>
+        /// <param name="categoria">Nueva categoria del producto</param>
+        /// <param name="informacionNutritiva">Nueva informacionNutritiva del producto</param>
+        /// <returns>Número de registros afectados</returns>
+        public int EditarProducto(MySqlConnection conexion, string codigo, string nombre, double precio, string descripcion, string categoria, string informacionNutritiva)
         {
+            string consulta;
+            consulta = string.Format("ALTER TABLE productos SET Nombre='{0}',Precio='{1}',Descripcion='{2}',Categoria='{3}',InformacionNutritiva='{4}'", nombre,
+                precio, descripcion, categoria, informacionNutritiva);
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            return comando.ExecuteNonQuery();
+        }
 
+        /// <summary>
+        /// Edita la información de un producto de la base de datos
+        /// </summary>
+        /// <param name="conexion">Conexión con la base de datos</param>
+        /// <param name="codigo">Código del producto</param>
+        /// <param name="nombre">Nuevo nombre del producto</param>
+        /// <param name="precio">Nuevo precio del producto</param>
+        /// <param name="descripcion">Nueva descripcion del producto</param>
+        /// <param name="categoria">Nueva categoria del producto</param>
+        /// <returns>Número de registros afectados</returns>
+        public int EditarProducto(MySqlConnection conexion, string codigo, string nombre, double precio, string descripcion, string categoria)
+        {
+            string consulta;
+            consulta = string.Format("ALTER TABLE productos SET Nombre='{0}',Precio='{1}',Descripcion='{2}',Categoria='{3}'", nombre,
+                precio, descripcion, categoria);
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            return comando.ExecuteNonQuery();
         }
     }
 }
